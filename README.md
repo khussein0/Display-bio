ba2f7fa8e2258d4dc034483c0cd70842cf9faa7d
khussein0 <49756345+khussein0@users.noreply.github.com>
2021-11-27 17:40:31 +0530

    initial-commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Display Bio Torre
+An application user interface to display bio for a Torre user.
+
+## Technologies:
+- HTML,CSS (Bootstrap)
+- Node.js (JavaScript ES6)
+- Express.js
+- NPM
+- AXIOS
+- GIT
+
+## Start
+Clone this repository with your git client.
+
+### Installation
+```shell
+> npm install
+```
+
+### Start App
+- For development purpose:
+```shell
+> npm run dev
+```
+- For production purpose:
+```shell
+> npm run prod
+```
+
+After starting the app we enter the url [http://localhost:3000/](http://localhost:3000/).
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+var router = express.Router();
+
+const endpoint = "https://torre.bio/api/bios/khussein20";
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  // call to API torre
+  await axios.get(endpoint)
+  .then( httpResponse => {
+      // if (httpResponse.data)        
+      const bios = httpResponse.data.person.picture;
+      res.locals.photo = bios;
+      res.locals.person = httpResponse.data.person;
+      res.locals.numExperiences = Object.entries(httpResponse.data.experiences).length;
+      res.locals.numJobs = Object.entries(httpResponse.data.jobs).length;
+      res.locals.numProjects = Object.entries(httpResponse.data.projects).length;
+      res.locals.numStrengths = Object.entries(httpResponse.data.strengths).length;
+      res.locals.numInterest = Object.entries(httpResponse.data.interests).length;
+      res.locals.numPublications = Object.entries(httpResponse.data.publications).length;
+      res.locals.numEducation = Object.entries(httpResponse.data.education).length;
+      res.locals.numLanguages = Object.entries(httpResponse.data.languages).length;
+      console.log(res.locals.person);
+      res.render('index.html');
+  }).catch(err => {
+       console.log('Error:', err);
+       next(err);
+  });
+    
+});
+
+
+module.exports = router;
